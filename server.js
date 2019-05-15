de45734ddd534eb83eb9115333f5df75ab2ab4e3
Alan2304 <sanchezalan95@gmail.com>
2019-05-15 12:28:15 -0500

    dumb fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/client-fmat-tour'));
+app.use(express.static(__dirname + '/dist/client_fmat_tour'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/client-fmat-tour/index.html'));
+res.sendFile(path.join(__dirname+'/dist/client_fmat_tour/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
